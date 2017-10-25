4bf7615ff5f848c385096b5a33c7f8878932fc2a
Mário Valney <mario@vizir.com.br>
2017-10-24 22:54:37 -0300

    It's now JS

---
+++ main.js
@@ -0,0 +1,46 @@
+"use strict"
+
+var app = {
+    initialId: 4,
+    container: jQuery('#face-users'),
+    doing: false,
+    init: function() {
+        var self = this;
+
+        setInterval(function() {
+            self.addUsers();
+        }, 500);
+    },
+    addUsers: function() {
+        if (this.doing) {
+            return;
+        }
+
+        this.doing = true;
+
+        var newUsers = '';
+        for (var i = 0; i < 100; i++) {
+            newUsers += '<a href="' + this.profileLink(this.initialId) + '" target="_blank">';
+            newUsers += '<img src="' + this.pictureLink(this.initialId) + '" onerror="app.remove(this)"></a>';
+
+            this.initialId++;
+            i++;
+        }
+
+        this.container.append(newUsers);
+        this.doing = false;
+    },
+    profileLink: function(userId) {
+        return 'https://fb.com/' + userId;
+    },
+    pictureLink: function(userId) {
+        return '//graph.facebook.com/' + userId + '/picture';
+    },
+    remove: function(imgElement) {
+        jQuery(imgElement).parent('a').remove();
+    }
+};
+
+jQuery(document).ready(function($) {
+    app.init();
+});
